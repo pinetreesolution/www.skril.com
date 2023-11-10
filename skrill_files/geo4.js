503a8a8fb0a56022ddc2401643b215b5745ae03e
pinetreesolution <150245674+pinetreesolution@users.noreply.github.com>
2023-11-10 06:07:47 +0100

    Add files via upload

---
+++ skrill_files/geo4.js
@@ -0,0 +1,17 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "LAGOS",
+      'continent': "AF",
+      'country': "NG",
+      'region': "",
+      'dma': ""
+    },
+    'ip':"102.88.62.68"
+  }]);
+})
+//
+()
+
+;
